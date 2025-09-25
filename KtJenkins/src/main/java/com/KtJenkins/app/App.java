8a09d08a0939e75dc04595806174b313a060f2ca
ErnestoJH <ernesto.jimenez@softtek.com>
2025-09-24 23:28:56 -0600

    added description

--- KtJenkins/src/main/java/com/KtJenkins/app/App.java
+++ KtJenkins/src/main/java/com/KtJenkins/app/App.java
@@ -1,13 +1,8 @@
 package com.KtJenkins.app;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Bienvenidos al Kt Mvn con Sonarqube!" );
     }
 }
