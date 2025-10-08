ec7163aeae37372fb375799577e1d32adbb9c2fe
ErnestoJH <ernesto.jimenez@softtek.com>
2025-10-08 15:07:56 -0600

    Kt

--- KtJenkins/src/main/java/com/KtJenkins/app/App.java
+++ KtJenkins/src/main/java/com/KtJenkins/app/App.java
@@ -1,13 +1,10 @@
 package com.KtJenkins.app;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos al Kt Mvn-Jenkins-Sonarqube!");
+        System.out.println("Este es un proyecto de prueba para integrar Jenkins y Sonarqube con Maven usando un pipilene de azure devops y un agente pool.");
+        System.out.println("Creado por:");
+        System.out.println("Ernesto Jimenez Huitron");
+        System.out.println("Softtek :)");
     }
 }
