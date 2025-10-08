c3a30704534a5d584d6a73bea3196b366b3aacc1
ErnestoJH <ernesto.jimenez@softtek.com>
2025-10-08 16:22:09 -0600

    generar archivo txt

--- KtJenkins/src/main/java/com/KtJenkins/app/App.java
+++ KtJenkins/src/main/java/com/KtJenkins/app/App.java
@@ -2,31 +2,44 @@ package com.KtJenkins.app;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class App {
+
     private static final Logger logger = Logger.getLogger(App.class.getName());
+    private static FileHandler fileHandler;
 
     public static void main(String[] args) {
-        try {
-            // Crear un FileHandler para escribir en "app.log", con append activado
-            FileHandler fileHandler = new FileHandler("app.log", true);
-            fileHandler.setFormatter(new SimpleFormatter()); // Formato legible
 
-            // Añadir el FileHandler al logger
+        try {
+            // Cambiamos aquí el nombre del archivo a Reports.txt
+            fileHandler = new FileHandler("Reports.txt", true);
+
+            fileHandler.setFormatter(new SimpleFormatter());
+
             logger.addHandler(fileHandler);
 
-            // Opcional: evitar que se impriman mensajes duplicados en consola
             logger.setUseParentHandlers(false);
+
+            logger.setLevel(Level.ALL);
+            fileHandler.setLevel(Level.ALL);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        logger.severe("Mensaje SEVERE (error grave)");
+        logger.warning("Mensaje WARNING (advertencia)");
         logger.info("Bienvenidos al Kt Mvn-Jenkins-Sonarqube!");
         logger.info("Este es un proyecto de prueba para integrar Jenkins y Sonarqube con Maven usando un pipeline de Azure DevOps y un agente pool.");
         logger.info("Creado por:");
         logger.info("Ernesto Jimenez Huitron");
         logger.info("Softtek :)");
+
+        if (fileHandler != null) {
+            fileHandler.close();
+        }
     }
 }
