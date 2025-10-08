a686e923bec27894615b7d094763e7b0c48589de
ErnestoJH <ernesto.jimenez@softtek.com>
2025-10-08 16:11:37 -0600

    Usamos logger.info(...) para mensajes informativos (que antes estaban en System.out.println)

--- KtJenkins/src/main/java/com/KtJenkins/app/App.java
+++ KtJenkins/src/main/java/com/KtJenkins/app/App.java
@@ -1,10 +1,16 @@
 package com.KtJenkins.app;
+
+import java.util.logging.Logger;
+
 public class App {
+    // Creamos el logger asociado a esta clase
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Bienvenidos al Kt Mvn-Jenkins-Sonarqube!");
-        System.out.println("Este es un proyecto de prueba para integrar Jenkins y Sonarqube con Maven usando un pipilene de azure devops y un agente pool.");
-        System.out.println("Creado por:");
-        System.out.println("Ernesto Jimenez Huitron");
-        System.out.println("Softtek :)");
+        logger.info("Bienvenidos al Kt Mvn-Jenkins-Sonarqube!");
+        logger.info("Este es un proyecto de prueba para integrar Jenkins y Sonarqube con Maven usando un pipeline de Azure DevOps y un agente pool.");
+        logger.info("Creado por:");
+        logger.info("Ernesto Jimenez Huitron");
+        logger.info("Softtek :)");
     }
 }
